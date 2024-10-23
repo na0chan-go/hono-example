e158a42f0b50924c61d49c2ab4f56fcf06220ac6
na0chan-go <nn19960218@gmail.com>
2024-10-23 23:02:25 +0900

    feat: エンドポイントの追加

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,13 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
+app.get('/test', (c) => {
+  return c.json({ message: 'This is a test' })
+})
+
+app.get('/api/v1/:name', (c) => {
+  const { name } = c.req.param()
+  return c.json({ message: `Hello ${name}` })
+})
+
 export default app
