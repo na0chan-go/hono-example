43fbbad3cbabbb029517acf148496ea45236bfce
na0chan-go <nn19960218@gmail.com>
2024-10-25 00:16:07 +0900

    feat: basic認証を追加

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,29 @@
 import { Hono } from 'hono'
 import { prettyJSON } from 'hono/pretty-json'
 import posts from './posts/posts'
+import auth from './auth/auth'
+import { basicAuth } from 'hono/basic-auth'
 
 const app = new Hono()
 
+// 全てのレスポンスをprettyJSONで整形する
 app.use('*', prettyJSON())
+
+// 認証が必要なルートを設定する
+app.use(
+  '/auth/*',
+  basicAuth({
+    username: 'admin',
+    password: 'password',
+  })
+)
+
+app.route('/posts', posts)
+app.route('/auth', auth)
 
 // cはcontextのこと
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
-
-app.route('/posts', posts)
 
 export default app
